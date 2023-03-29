fc433b147d461c4e49df5f7a5a36807c8aaa8bcc
Anatoliy <127441931+Lapanca@users.noreply.github.com>
2023-03-29 04:06:07 +0300

    Growfy

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('click',documentClick);
+
+function documentClick(e){
+    const targetitem = e.target;
+
+    if (targetitem.closest('.icon-menu')){
+        document.documentElement.classList.toggle('menu-open')
+    }
+}
